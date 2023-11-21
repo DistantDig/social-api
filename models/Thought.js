68a593b8ada69e8ec83e560add38a86947c27e04
DistantDig <jameswaller711@gmail.com>
2023-11-21 14:42:08 -0800

    added though model

---
+++ models/Thought.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction]
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;

9425ac6a2ceacac3a5b10c982f0495d9ca85fdbd
DistantDig <jameswaller711@gmail.com>
2023-11-24 14:18:09 -0800

    created thoughts are now appended to user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 module.exports = {
     // Get all thoughts
@@ -29,8 +29,19 @@ module.exports = {
     // Creates a new thought
     async createThought (req, res) {
         try {
-            const thoughts = await Thought.create(req.body);
-            res.json(thoughts);
+            const thought = await Thought.create(req.body);
+
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id }},
+                { runValidators: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID'});
+            }
+            
+            res.json(thought);
         } catch (err) {
             res.status(500).json(err);
         }
